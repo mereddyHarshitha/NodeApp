6b3ad70459f6eead95e217e4f18feafdb405132e
mereddyHarshitha <mereddy.harshitha@gmail.com>
2020-02-21 13:28:04 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var nforce = require('nforce');
 var express = require('express');
-var cors = require('cors');
 var port = process.env.PORT || 3000;
 
 var org = nforce.createConnection({
@@ -13,15 +12,7 @@ var org = nforce.createConnection({
 });
 
 var app = express();
-app.use(cors());
-app.use(function(req, res, next) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Response-Time, X-PINGOTHER, X-CSRF-Token,Authorization,X-Authorization');
-      res.setHeader('Access-Control-Allow-Methods', '*');
-      res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
-      res.setHeader('Access-Control-Max-Age', '1000');
-      next();
-  });
+
 // Require Routes js
 var routesHome = require('./routes/home');
 
