8027cb3e95bba76bf6b22f3c690ea39566f5b93a
mereddyHarshitha <mereddy.harshitha@gmail.com>
2020-02-20 23:47:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 
 var org = nforce.createConnection({
-  clientId: '3MVG9uudbyLbNPZMk0vYn7ICarLW4qV5bLdL.KqYws.i1.oN99y14Skth6utXg0nwCuPpSMtr9lB7HIOx6M65',
-  clientSecret: '6326007125179395206',
-  redirectUri: 'https://protected-fortress-46904.herokuapp.com/oauth/_callback',
-  apiVersion: 'v34.0',  // optional, defaults to current salesforce API version
+  clientId: '3MVG9n_HvETGhr3AerhMPQWA3Z_X_pZduM18nC6jICZEY3ZAJynYZSnga4b3rMWiNgtj6II4of3kcEe.vgLL9',
+  clientSecret: '28DD8FEB6824FBFFA664936E73A0359EACE498F595BC7DCCF0D7B352677F6974',
+  redirectUri: 'https://lwcopplist.herokuapp.com/oauth/_callback',
+  apiVersion: 'v47.0',  // optional, defaults to current salesforce API version
   environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
   mode: 'multi' // optional, 'single' or 'multi' user mode, multi default
 });
@@ -32,7 +32,7 @@ app.get('/oauth/_callback', function(req, res) {
     if(!err) {
       console.log('Access Token: ' + resp.access_token);
       app.locals.oauthtoken = resp.access_token;
-      app.locals.lightningEndPointURI = "https://sedreambmo-dev-ed.lightning.force.com";
+      app.locals.lightningEndPointURI = "https://lightning-out-developer-edition.ap16.force.com";
       res.redirect('/home');
     } else {
       console.log('Error: ' + err.message);
